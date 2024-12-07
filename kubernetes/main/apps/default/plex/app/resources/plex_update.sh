fc184dfc3eaa61bd5e301f63d032002ce9b482da
mmberry96 <mmberry96@gmail.com>
2024-12-07 00:03:51 -0600

    Migrate plex to ExternalSecrets

---
+++ kubernetes/main/apps/default/plex/app/resources/plex_update.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# PLEX PARTIAL SCAN script or PLEX UPDATE script
+# When zurg detects changes, it can trigger this script IF your config.yml contains
+# on_library_update: sh plex_update.sh "$@"
+
+# docker compose exec zurg apk add libxml2-utils
+# sudo apt install libxml2-utils
+
+if [ -z "${PLEX_URL}" ]; then
+  echo "Missing environment variable PLEX_URL"
+  exit 1
+fi
+
+if [ -z "${PLEX_TOKEN}" ]; then
+  echo "Missing environment variable PLEX_TOKEN"
+  exit 1
+fi
+
+plex_url="$PLEX_URL"
+token="$PLEX_TOKEN" # open Plex in a browser, open dev console and copy-paste this: window.localStorage.getItem("myPlexAccessToken")
+zurg_mount="${ZURG_MOUNT:-/mnt/zurg}" # replace with your zurg mount path, ensure this is what Plex sees
+
+# Get the list of section IDs
+section_ids=$(curl -sLX GET "$plex_url/library/sections" -H "X-Plex-Token: $token" | xmllint --xpath "//Directory/@key" - | grep -o 'key="[^"]*"' | awk -F'"' '{print $2}')
+
+for arg in "$@"
+do
+    parsed_arg="${arg//\\}"
+    echo "$parsed_arg"
+    modified_arg="$zurg_mount/$parsed_arg"
+    echo "Detected update on: $arg"
+    echo "Absolute path: $modified_arg"
+
+    for section_id in $section_ids
+    do
+        echo "Section ID: $section_id"
+
+        curl -G -H "X-Plex-Token: $token" --data-urlencode "path=$modified_arg" "$plex_url"/library/sections/"$section_id"/refresh
+    done
+done
+
+echo "All updated sections refreshed"
+
+# credits to godver3, wasabipls
